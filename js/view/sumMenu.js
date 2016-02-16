ab83000948da792a59eb3f18369bbe8f088aba08
Ariel Blomkvist Rova <arielr@kth.se>
2016-02-16 14:33:25 +0100

    Center med BS-offset

--- js/view/sumMenu.js
+++ js/view/sumMenu.js
@@ -11,7 +11,7 @@ var SumMenu = function (container, model) {
 
     for (var i = 0; i < menu.length; i++) {
         dishesDiv.append("<div class='col-md-2 menuDish' style='border: solid #000000; padding:10px; margin-left:20px;'>"
-            + "<img style='width:180px; height:100px;' src=\"images/" + menu[i].image + "\"" + ">"
+            + "<img style='width:100%; height:100px;' src=\"images/" + menu[i].image + "\"" + ">"
             + "<p style='background:#995; text-align:center;'>" + menu[i].name + "</p>"
             + "<p style='font-weight:bold; text-align:center;'>" + model.getDishPrice(menu[i]) + " SEK" + "</p>");
     }
